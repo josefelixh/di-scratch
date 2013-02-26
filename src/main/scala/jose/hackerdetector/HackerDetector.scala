0503b674ca0d066715a39370bbc56f326893861f
josefelixh <josefelixh@gmail.com>
2013-02-26 02:31:21 +0000

    heave refactoring, looking for the style

--- src/main/scala/jose/hackerdetector/HackerDetector.scala
+++ src/main/scala/jose/hackerdetector/HackerDetector.scala
@@ -7,40 +7,53 @@ trait HackerDetectorImpl extends HackerDetector {
   
   case class SIGNIN_FAILURE(ip: String, time: Int, user: String)
   
-  val failures = collection.concurrent.TrieMap.empty[Int, List[SIGNIN_FAILURE]]
-  
-  override def parseLine(line: String) = check (parsed (line))
-  
-  def now = System.currentTimeMillis() / 1000 //in seconds
-  val window = 5 * 60 
-  def windowAgo = now - window 
+  val window = 5 * 60 //5 minutes in seconds 
   val failureThresold = 5
   
-  val inWindow: ((Int, _)) => Boolean = { case (time, _) => time > windowAgo }
+  val failures = collection.concurrent.TrieMap.empty[Int, List[SIGNIN_FAILURE]]
+  def failuresWindow = failures.keys.toList.sorted
+  def failuresInWindow = failures.values.flatten
   
-  val suspicious: SIGNIN_FAILURE => Boolean = { failure =>
-    failures.values.flatten.filter(_.ip == failure.ip).size >= failureThresold 
-  }
-  
-  val check: Option[SIGNIN_FAILURE] => String = _ map { failure =>
-    if (suspicious(failure)) failure.ip else null
+  override def parseLine(line: String) = parse (line) map { failure =>
+    purgeFailuresNotInWindow(failure.time)
+    register(failure)
+    if (suspicious(failure)) {
+      purgeRedundantFailures(failure.ip)
+      failure.ip 
+    } else null
   } getOrElse(null)
   
-  val register: SIGNIN_FAILURE => SIGNIN_FAILURE = { signinFailure =>
-    failures.filterNot { inWindow }.keys.toList.foreach { failures.remove }
+  def purgeFailuresNotInWindow(time: Int) {
+    failuresWindow filterNot { inWindow(time) } foreach { failures.remove }
+  }
+  
+  def purgeRedundantFailures(ip: String) {
+    val failureTimes = failures.filter { case (_, list) => list.exists(_.ip == ip) }.keys
+    val olderFailure = failureTimes.toList.sorted.head 
+    val (matched, others) = failures(olderFailure).partition(_.ip == ip)
+    
+    failures.update(olderFailure, others ::: matched.tail)
+  }
+  
+  def inWindow(time: Int): Int => Boolean =  _ > (time - window)
+  
+  def register(signinFailure: SIGNIN_FAILURE) {
     failures.get(signinFailure.time) map { currentList =>
-      failures.update(signinFailure.time, signinFailure :: currentList.sortBy(_.time) take (failureThresold - 1))
-    } getOrElse (failures.put(signinFailure.time, signinFailure :: Nil))
-    signinFailure
+      failures.update(signinFailure.time, signinFailure :: currentList)
+    } getOrElse {
+      failures.put(signinFailure.time, signinFailure :: Nil)
+    }
+  }
+  
+  val suspicious: SIGNIN_FAILURE => Boolean = { failure =>
+    failuresInWindow.filter(_.ip == failure.ip).size == failureThresold 
+  }
+  
+  val parse: String => Option[SIGNIN_FAILURE] = _ split (",") match {
+      case x if notValid(x) => throw new IllegalArgumentException
+      case Array(ip, failureTime, "SIGNIN_FAILURE", user) => Some(SIGNIN_FAILURE(ip, failureTime.toInt, user))
+      case _ => None
   }
   
   val notValid: Array[_] => Boolean = _.length != 4
-  
-  val parsed: String => Option[SIGNIN_FAILURE] = _ split (",") match {
-      case x if notValid(x) => throw new IllegalArgumentException
-      case Array(ip, failureTime, "SIGNIN_FAILURE", user) if inWindow((failureTime.toInt, None)) => 
-        Some(register(SIGNIN_FAILURE(ip, failureTime.toInt, user)))
-      case _ => None
-  }
 }
-
--- src/test/scala/jose/hackerdetector/HackerDetectorTest.scala
+++ src/test/scala/jose/hackerdetector/HackerDetectorTest.scala
@@ -6,15 +6,13 @@ import org.scalatest.matchers.ShouldMatchers
 class HackerDetectorTest extends FlatSpec with ShouldMatchers {
 
   "The HackerDetector" should "not return suspicious ip (normal activity)" in new HackerDetectorImpl {
-    override val now = 0.toLong
-    val testData = for (host <- 1 to 254) yield s"192.168.0.$host,0,SIGNIN_FAILURE,John Moore"
+    val testData = for (host <- 1 to 254) yield s"192.168.0.$host,$host,SIGNIN_FAILURE,John Moore"
     testData.par map {
       parseLine(_) should be(null)
     }
   }
 
   it should "return suspicious ip (more than 5 failures within 5 minutes)" in new HackerDetectorImpl {
-    override val now = 301.toLong
     val host = 1
     parseLine(s"192.168.0.$host,150,SIGNIN_FAILURE,John Moore") should be(null)
     parseLine(s"192.168.0.$host,150,SIGNIN_FAILURE,John Moore") should be(null)
@@ -22,11 +20,23 @@ class HackerDetectorTest extends FlatSpec with ShouldMatchers {
     parseLine(s"192.168.0.$host,150,SIGNIN_FAILURE,John Moore") should be(null)
     parseLine(s"192.168.0.$host,150,SIGNIN_FAILURE,John Moore") should be("192.168.0.1")
     parseLine(s"192.168.0.$host,150,SIGNIN_FAILURE,John Moore") should be("192.168.0.1")
-    parseLine(s"192.168.0.$host,0,SIGNIN_FAILURE,John Moore") should be(null)
+    parseLine(s"192.168.0.$host,500,SIGNIN_FAILURE,John Moore") should be(null)
   }
 
   it should "throw IllegalArgumentException if entry is invalid" in new HackerDetectorImpl {
     evaluating { parseLine("Hello World!") } should produce [IllegalArgumentException]
+    
     evaluating { parseLine("127.0.0.1,invalid,SIGNIN_FAILURE,John Moore") } should produce [IllegalArgumentException]
+  }
+  
+  it should "purge redundant failures" in new HackerDetectorImpl {
+    parseLine(s"192.168.0.1,150,SIGNIN_FAILURE,John Moore") should be(null)
+    parseLine(s"192.168.0.1,150,SIGNIN_FAILURE,John Moore") should be(null)
+    parseLine(s"192.168.0.1,150,SIGNIN_FAILURE,John Moore") should be(null)
+    parseLine(s"192.168.0.1,150,SIGNIN_FAILURE,John Moore") should be(null)
+    parseLine(s"192.168.0.1,150,SIGNIN_FAILURE,John Moore") should be("192.168.0.1")
+    failures(150).size should be (4)
+    parseLine(s"192.168.0.1,150,SIGNIN_FAILURE,John Moore") should be("192.168.0.1")
+    failures(150).size should be (4)
   }
 }
